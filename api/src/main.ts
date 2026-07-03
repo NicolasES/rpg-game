34883ffd3464c613088c2ef547e2b4ce79f45b3b
NicolasES <nicolas.escouto@gmail.com>
2026-07-03 11:27:55 -0300

    docs: add SwaggerModule

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,11 +1,19 @@
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DomainExceptionFilter } from './shared/infrastructure/filters/DomainExceptionFilter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('API routes')
+    .setDescription('Api routes list')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useGlobalFilters(new DomainExceptionFilter());
