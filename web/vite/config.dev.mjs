86802de01757f86c5f629cdcda9305da3c543a2b
NicolasES <nicolas.escouto@gmail.com>
2026-07-03 15:12:26 -0300

    chore: restructure docker-compose and update vite dev config

--- web/vite/config.dev.mjs
+++ web/vite/config.dev.mjs
@@ -10,6 +10,10 @@ export default defineConfig({
         tailwindcss(),
     ],
     server: {
+        host: '0.0.0.0',
+        hmr: {
+            host: 'localhost',
+        },
         port: 8080
     }
 })
